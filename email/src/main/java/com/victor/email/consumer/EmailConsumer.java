6c58afb68d32eae745e044daf1f1f9d0e1d5a423
Victor Marques <victor.marques.98v@gmail.com>
2026-04-04 13:47:33 -0300

    feat: Email consumer passa a receber o payload completo e salvar como objeto no banco de dados

--- email/src/main/java/com/victor/email/consumer/EmailConsumer.java
+++ email/src/main/java/com/victor/email/consumer/EmailConsumer.java
@@ -1,15 +1,34 @@
 package com.victor.email.consumer;
 
 import com.victor.email.dto.EmailDto;
+import com.victor.email.enums.EmailStatus;
+import com.victor.email.model.EmailModel;
+import com.victor.email.repository.EmailRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class EmailConsumer {
 
+    private final EmailRepository emailRepository;
+
+    public EmailConsumer(EmailRepository emailRepository) {
+        this.emailRepository = emailRepository;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailDto emailDto){
-        System.out.println(emailDto.emailTo());
+        EmailModel emailModel = new EmailModel();
+        emailModel.setUserId(emailDto.id());
+        emailModel.setEmailTo(emailDto.emailTo());
+        emailModel.setEmailSubject(emailDto.subject());
+        emailModel.setEmailBody(emailDto.body());
+        emailModel.setStatusEmail(EmailStatus.PENDING);
+        emailModel.setSendDateEmail(LocalDateTime.now());
+
+        emailRepository.save(emailModel);
     }
 }
